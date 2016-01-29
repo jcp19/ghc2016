76a469fd7f0c98bebad53dcadb6af50a180dc0b7
Joao Pereira <joaopereira_19@hotmail.com>
2016-01-29 18:26:57 +0000

    servers.h updated, added abstractions

--- qualification2015/servers.h
+++ qualification2015/servers.h
@@ -1,12 +1,33 @@
+typedef int NUM;
+
 typedef struct server {
-    int size, capcity;
+    NUM size, capcity;
 } * SERVER;
 
-SERVER newServer ( int size, int capacity )
+SERVER newServer ( NUM size, NUM capacity )
 {
-    SERVER newServ = malloc (sizeof(struct server));
+    SERVER newServ = malloc( sizeof( struct server ) );
     newServer -> size = size;
     newServer -> capacity = capacity;
     return newServer;
 }
 
+/*
+typedef struct pool {
+    NUM * cap_by_row; // Stores the capacity of the pool that comes from each row
+} POOL;
+*/
+
+typedef ( NUM * ) POOL; // Stores the capacity of the pool that comes from each row
+
+POOL newPool ( NUM rows )
+{
+    return malloc( sizeof( NUM ) * rows );
+}
+
+typedef ( NUM * ) DATACENTER; // must be seen as a matrix that stores the info of the row
+
+DATACENTER newDataCenter ( NUM rows, NUM columns )
+{
+    return calloc( sizeof( NUM ) * rows * columns );
+}
