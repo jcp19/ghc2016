e661f4be6d80d26bf86a7edbc0d2e6fd59490411
Joao Pereira <joaopereira_19@hotmail.com>
2016-02-02 02:15:31 +0000

    adicionadas algumas funções para inserir os servidores na lista

--- qualification2015/ghashcode.hs
+++ qualification2015/ghashcode.hs
@@ -1,7 +1,9 @@
+import Data.List
+
 module Main where
 
-data Server = Vazio | Ocupado | Serv Int Int Int deriving (Show, Read, Eq)
--- id capacity size 
+data Server = Vazio | Ocupado | Serv Int Int Int Int Int Int deriving (Show, Read, Eq)
+-- id capacity size x y pool
 type DataCenter = [[Server]]
 
 newDataCenter :: Int -> Int -> DataCenter
@@ -9,7 +11,7 @@ newDataCenter x y = replicate x (replicate y Vazio)
 
 -- insere na posição escolhida do DC, nao verifica se é possivel inserir na posiçao escolhida, isso deve ser verificado antes
 insertPosDataCenter :: DataCenter -> Int -> Int -> Server -> DataCenter
-insertPosDataCenter dc x y a@(Serv id cap size) = (take x dc) ++ [take y (dc!!x) ++ [a] ++ (replicate (size-1) Ocupado) ++ drop (y+size) (dc!!x)] ++ (drop (x+1) dc)
+insertPosDataCenter dc x y a@(Serv id cap size _ _ _) = (take x dc) ++ [take y (dc!!x) ++ [(Serv id cap size x y (-1))] ++ (replicate (size-1) Ocupado) ++ drop (y+size) (dc!!x)] ++ (drop (x+1) dc)
 insertPosDataCenter dc x y a = (take x dc) ++ [take y (dc!!x) ++ [a] ++ drop (y+1) (dc!!x)] ++ (drop (x+1) dc)
 
 insereOcupados :: Int -> DataCenter -> IO DataCenter
@@ -23,9 +25,29 @@ insereOcupados x dc = do pos <- getLine
 
 leServers :: Int -> [String] -> [Server]
 leServers _ [] = []
-leServers x (h:t) = (Serv x (vars!!1) (vars!!0)) : (leServers (x+1) t)
+leServers x (h:t) = (Serv x (vars!!1) (vars!!0) (-1) (-1) (-1)) : (leServers (x+1) t)
 	               where
 	               	 vars = (map read (words h)) :: [Int]
+
+indexer :: [a] -> [(a,Int)]
+indexer x = indexerAux x 0
+              where indexerAux [] _ = []
+                    indexerAux (h:t) x = ((h,x):(indexerAux t (x+1)))
+
+linha_menorCap_ondeCabe :: DataCenter -> Server -> Int
+-- falta ordenar por capacidade
+linha_menorCap_ondeCabe dc (Serv _ _ size _ _ _) =  head (sort (filter (\(x,y) -> maxSlotFree x >= size) (indexer dc)))
+
+maxSlotFree :: [Servers] -> Int
+maxSlotFree [] = 0
+maxSlotFree k = max( (length (takeWhile (\x -> x == Vazio)) k), (maxSlotFree (DropWhile (\x -> x /= Vazio) (DropWhile (\x -> x == Vazio) ) k)) )
+
+fillDataCenter :: DataCenter -> [Servers] -> DataCenter
+-- dado o data center, enche-o com os servidores (a lista de Servers está ordenada decrescentemente por racio)
+fillDataCenter dc [] = dc
+fillDataCenter dc (h:t) | linha_menorCap_ondeCabe dc h == -1 = fillDataCenter dc t
+                        | otherwise = insertPosDataCenter dc (linha_menorCap_ondeCabe dc h) (melhor_pos dc (linha_menorCap_ondeCabe dc h) h) h
+
 
 main = do primeiraLinha <- getLine
           vars <- return ((map (read) (words primeiraLinha)) :: [Int])
