72afb95ddb924a1d207ea21a8a76cc7ce77f1c7d
Joao Pereira <joaopereira_19@hotmail.com>
2016-02-09 22:17:47 +0000

    update main.hs

--- example2016/src/main.hs
+++ example2016/src/main.hs
@@ -9,11 +9,11 @@ type Position = ((Int, Int), (Int, Int))
 newCanvas :: Int -> Int -> Canvas
 newCanvas x y = replicate x (replicate y '.')
 
-insertPosCanvas :: Canvas -> Int -> Int -> Char -> Canvas
-insertPosCanvas canvas x y char = (take x canvas) ++ [take y (canvas!!x) ++ [char] ++ drop (y+1) (canvas!!x)] ++ (drop (x+1) canvas)
+insertPosCanvas :: Canvas -> Position -> Char -> Canvas
+insertPosCanvas canvas (x,y) char = (take x canvas) ++ [take y (canvas!!x) ++ [char] ++ drop (y+1) (canvas!!x)] ++ (drop (x+1) canvas)
 
-eraseCell :: Canvas -> Int -> Int -> Canvas
-eraseCell canvas x y = insertPosCanvas canvas x y '.'
+eraseCell :: Canvas -> Position -> Canvas
+eraseCell canvas (x,y) = insertPosCanvas canvas (x,y) '.'
 
 readCanvas :: IO Canvas
 readCanvas = do board <- getContents
@@ -23,6 +23,12 @@ bestColumn :: Canvas -> (Position, Position)
 -- computes the best column in which we can paint a line, the output is in the same format as the input for the PAINT_LINE
 bestColumn = invert$transpose$bestLine
                 where invert = \((a,b), (c,d)) -> ((b,a), (d,c)) 
+
+paintLine :: Canvas -> (Position, Position) -> Canvas
+paintLine canvas ((x,y), (a,b)) | x > a || y > b = canvas
+                                | x == a = paintLine (insertPosCanvas canvas (x,y) '#') ((x,y+1), (a,b))
+                                | y == b = paintLine (insertPosCanvas canvas (x,y) '#') ((x+1,y), (a,b))
+                                | otherwise = error "You Fucked UP"
 
 main = do dims <- getLine
           dims <- return ((map (read) (words dims)) :: [Int])
