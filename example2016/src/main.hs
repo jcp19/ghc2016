76ade0ebe7977c529b5b475820caf55cd0f5925b
Joao Pereira <joaopereira_19@hotmail.com>
2016-02-09 21:16:05 +0000

    we are able to read the input

--- example2016/src/main.hs
+++ example2016/src/main.hs
@@ -3,7 +3,25 @@ module Main where
 import Data.List
 import Data.Function
 
---tipos de dados
 type Canvas = [[Char]]
 
-main = do
+newCanvas :: Int -> Int -> Canvas
+newCanvas x y = replicate x (replicate y '.')
+
+insertPosCanvas :: Canvas -> Int -> Int -> Char -> Canvas
+insertPosCanvas canvas x y char = (take x canvas) ++ [take y (canvas!!x) ++ [char] ++ drop (y+1) (canvas!!x)] ++ (drop (x+1) canvas)
+
+eraseCell :: Canvas -> Int -> Int -> Canvas
+eraseCell canvas x y = insertPosCanvas canvas x y '.'
+
+readCanvas :: IO Canvas
+readCanvas = do board <- getContents
+                return$lines board
+
+main = do dims <- getLine
+          dims <- return ((map (read) (words dims)) :: [Int])
+          (rows, columns) <- return (dims!!0, dims!!1)
+          canvas <- readCanvas
+          print canvas
+          
+
