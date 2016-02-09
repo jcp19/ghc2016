d3a2887309178d84a499a06bcb37ce93591536af
Joao Pereira <joaopereira_19@hotmail.com>
2016-02-09 23:25:50 +0000

    compila e o formato é o pretendido

--- example2016/src/main.hs
+++ example2016/src/main.hs
@@ -28,30 +28,45 @@ bestColumn a = invert (bestRow$transpose a)
 
 paintLine :: Canvas -> Char -> (Position, Position) -> Canvas
 paintLine canvas c ((x,y), (a,b)) | x > a || y > b = canvas
-                                  | x == a = paintLine (insertPosCanvas canvas (x,y) c) ((x,y+1), (a,b))
-                                  | y == b = paintLine (insertPosCanvas canvas (x,y) c) ((x+1,y), (a,b))
+                                  | x == a = paintLine (insertPosCanvas canvas (x,y) c) c ((x,y+1), (a,b))
+                                  | y == b = paintLine (insertPosCanvas canvas (x,y) c) c ((x+1,y), (a,b))
                                   | otherwise = error "You Fucked UP"
 
 paintItAux :: Canvas -> Canvas -> [String] -> (Canvas, [String])
 -- given the original canvas and an empty "history" of Comands, it computes de functions needed. this function should be connected to one of type Canvas -> [String] that returns only the history
 -- an empty canvas must be passed as a 2nd argument for simplicity's sake
 paintItAux canvas empty hist | canvas == empty = ([], hist)
-                             | helpY bc >= helpX br = (paintLine canvas '.' bc, hist ++ (newRec bc))
-                             | otherwise = (paintLine canvas '.' br, hist ++ (newRec br))
+                             | helpY bc >= helpX br = paintItAux (paintLine canvas '.' bc) empty (hist ++ (newRec bc)) 
+                             | otherwise = paintItAux (paintLine canvas '.' br) empty (hist ++ (newRec br))
                                  where
-                                 	br = bestRow canvas
+                                    br = bestRow canvas
                                     bc = bestColumn canvas
                                     helpY = (\((a,b), (c,d)) -> c - a)
                                     helpX = (\((a,b), (c,d)) -> d - b)
                                     newRec ((a,b),(c,d)) = ["PAINT_LINE " ++ show a++ " " ++ show b ++ " " ++ show c ++ " " ++ show d]
 
-paintIt :: Canvas -> [String]
+paintIt :: Canvas -> String
 paintIt canvas | length canvas == 0 = []
-               | otherwise = snd(paintItAux canvas (emptyCanvas (length canvas) (length (head canvas))) []) 
+               | otherwise = unlines$snd(paintItAux canvas (emptyCanvas (length canvas) (length (head canvas))) []) 
 
 main = do dims <- getLine
           dims <- return ((map (read) (words dims)) :: [Int])
           (rows, columns) <- return (dims!!0, dims!!1)
           canvas <- readCanvas
-          print (paintIt canvas)
+          putStrLn (paintIt canvas)
           
+--JM
+
+bestRow :: Canvas -> (Position,Position)
+bestRow canvas = snd $ bestRowSeg
+    where bestRowSeg = maximumBy (compare `on` (length . fst)) (potentialRowSegs canvas (0,0))
+
+potentialRowSegs :: Canvas -> Position -> [(String, (Position, Position))]
+potentialRowSegs [] _ = []
+potentialRowSegs ([]:t) (x,y) = potentialRowSegs t (x+1, 0)
+potentialRowSegs l@(h:t) (x,y) | hashtags == []     = potentialRowSegs t (x+1, 0)
+                               | otherwise          = (hashtags, ((x,yStart),(x,yEnd))) : (potentialRowSegs (rest':t) (x,yEnd+1)) 
+    where (dots, rest) = span (=='.') h
+          (hashtags, rest') = span (=='#') rest
+          yStart = y + length dots
+          yEnd = yStart + length hashtags - 1
