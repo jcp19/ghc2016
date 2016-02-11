f8382c854daa88d3dd9009d494ff64d95a7bb03e
linuxPenguin <a68646@alunos.uminho.pt>
2016-02-11 19:05:08 +0000

    Added data types.

---
+++ qualification2016/jm.hs
@@ -0,0 +1,11 @@
+data State = Waiting Load | Loading Load | Delivery Position Load 
+
+type Order = (ID, Position, Load)
+type Wherehouse = (ID, Position, Load, [Drone])
+type Drone = (ID, Position, State) Load
+
+type ID = Int
+type Product = Int
+type Quantity = Int
+type Position = (Int,Int)
+type Load = [(Product, Quantity)]
