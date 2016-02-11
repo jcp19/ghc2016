5cfd55d813f7a6d626663aeda359b624643b7efb
linuxPenguin <a68646@alunos.uminho.pt>
2016-02-11 19:47:08 +0000

    Data type corrections and new functions.

--- qualification2016/jm.hs
+++ qualification2016/jm.hs
@@ -1,7 +1,8 @@
-data State = Waiting Contents | Loading Contents | Delivery Position Contents 
+data State = Waiting Contents | Loading Contents | Delivery Position Contents
+    deriving (Eq,Ord,Show)
 
 type Order = (ID, Position, Contents)
-type Wherehouse = (ID, Position, Contents, [Drone])
+type Warehouse = (ID, Position, Contents, [Drone])
 type Drone = (ID, Position, State)
 
 type ID = Int
@@ -9,4 +10,18 @@ type Product = Int
 type Quantity = Int
 type Position = (Int,Int)
 type Contents = [(Product, Quantity)]
-type History = [String]
+type TurnCount = Int
+type History = ([String], TurnCount)
+
+distance :: Position -> Position -> Int
+distance (x1,y1) (x2,y2) = let c1 = (x1 - x2) ^ 2
+                               c2 = (y1 - y2) ^ 2
+                           in (ceiling $ sqrt $ fromIntegral (c1 + c2))
+
+filterByProximity :: [Order] -> Position -> Int -> [Order]
+filterByProximity orders position maxDist = filter (\ (_,orderPos,_) -> distance orderPos position <= maxDist) orders
+
+filterWaiting :: [Drone] -> [Drone]
+filterWaiting [] = []
+filterWaiting (drone@(_,_,Waiting _):remaining) = drone : filterWaiting remaining
+filterWaiting (_:t) = filterWaiting t
